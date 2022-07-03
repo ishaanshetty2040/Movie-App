8ff29b748d3a0be5b9187afe0acd1ae070e83f58
Ishaan <ishaanshetty12@gmail.com>
2022-07-04 00:44:59 +0530

    Files modified

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./Components/MovieList";
+import MovieListHeading from "./Components/MovieListHeading";
+import SearchBox from "./Components/SearchBox";
 /* import './App.css'; */
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      Title: "Star Wars",
-      Year: "1977",
-      imdbID: "tt0076759",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode V - The Empire Strikes Back",
-      Year: "1980",
-      imdbID: "tt0080684",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode VI - Return of the Jedi",
-      Year: "1983",
-      imdbID: "tt0086190",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-  const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=star wars&apikey=e9d1a297";
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=e9d1a297`;
     const response = await fetch(url);
     const responseJSON = await response.json();
+    if (responseJSON.Search) {
+      setMovies(responseJSON.Search);
+    }
   };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [
+    searchValue,
+  ]); /* We add emty array,which means we request movies,when page gets loaded only  */
+
   return (
     <div className="container-fluid movie-app">
+      <div className="row">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
